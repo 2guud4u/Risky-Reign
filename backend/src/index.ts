3a735c33ed9a46df839043c74db73bf01aecea22
Jiahua Jiang <jiahuajiang@jiahuas-MacBook-Air.local>
2025-07-13 19:04:47 -0700

    successfully script sharing start

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -204,6 +204,7 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3001;
+console.log("Server is starting...");
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
